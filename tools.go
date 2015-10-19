5ffdeefb77af82417385c6d0d022e618a02a751d
Christian Wolfgang <wolfgarnet@gmail.com>
2015-10-19 16:43:08 +0200

    [TPL-3] Returning HTML

--- tools.go
+++ tools.go
@@ -3,33 +3,34 @@ package templates
 import (
 	"bytes"
 	"reflect"
+	"html/template"
 )
 
 type Tools struct {
 	renderer *Renderer
 }
 
-func (t Tools) Render(object interface{}, view string, trySuper bool) string {
+func (t Tools) Render(object interface{}, view string, trySuper bool) template.HTML {
 	logger.Debug("Tool render {}, {}", object, view)
 	renderer, err:= t.renderer.manager.RenderObject(t.renderer.ThemeName, t.renderer.PackageName, object, view, trySuper)
 
 	if err != nil {
-		return err.Error()
+		return template.HTML(err.Error())
 	}
 
 	b := new(bytes.Buffer)
 	renderer.Render(b)
-	return b.String()
+	return template.HTML(b.String())
 }
 
-func (t Tools) RenderStatic(tt reflect.Type, view string) string {
+func (t Tools) RenderStatic(tt reflect.Type, view string) template.HTML {
 	renderer, err := t.renderer.manager.RenderType(t.renderer.ThemeName, t.renderer.PackageName, tt, view)
 
 	if err != nil {
-		return err.Error()
+		return template.HTML(err.Error())
 	}
 
 	b := new(bytes.Buffer)
 	renderer.Render(b)
-	return b.String()
+	return template.HTML(b.String())
 }
